8685d2fc3a25ea2310cb9d4dd9eea36b882d757f
Suresh R P <rpsuresh05@gmail.com>
2024-07-14 16:57:48 -0500

    code: gestures

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import BasicsOfMotion from './BasicsOfMotion'
+import Gestures from './Gestures'
 
 function App() {
 
   return (
     <>
-      <BasicsOfMotion />
+      {/* <BasicsOfMotion /> */}
+      <Gestures/>
     </>
   )
 }
